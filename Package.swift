aab5adb0d4ef455c60497b41eca2dacb84420e01
Johnathon Karcz <jkarcz@sapient.com>
2020-10-09 15:05:22 -0500

    about to change stuff up

--- Package.swift
+++ Package.swift
@@ -5,11 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "Camiitool",
+    //     platforms: [
+    //     .iOS(.v13),         //.v8 - .v13
+    //     .macOS(.v10_15),    //.v10_10 - .v10_15
+    //     .tvOS(.v13),        //.v9 - .v13
+    //     .watchOS(.v6),      //.v2 - .v6
+    // ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Camiitool",
-            targets: ["Camiitool"]),
+            targets: ["amiitool","Camiitool", "mbedtls"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,17 +25,64 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Camiitool",
-            dependencies: [],
+            name: "amiitool",
+            dependencies: ["mbedtls"],
+
             path: "./Sources/amiitool",
-            exclude: ["./mbedtls/yotta/"],
+            // path: "Modules",
+
+            exclude: [
+                "./mbedtls/yotta/",
+                "./mbedtls/visualc",
+                "LICENSE"
+            ],
+
+            resources: [
+                .copy("."),
+                // .copy("include/"),
+                // .copy("mbedtls"),
+                // .copy("README.md"),
+                // .copy("Makefile")
+            ],
+            // publicHeadersPath: "./mbedtls/include/",
             cSettings: [
-                .headerSearchPath("./mbedtls/include/"),
+                // .headerSearchPath("./mbedtls/include/"),
                 .headerSearchPath("./include/")]
+        ),
+        .target(
+            name: "mbedtls",
+            dependencies: [],
+
+            // path: "./Sources/amiitool",
+            path: "./Sources/amiitool/mbedtls",
+
+            exclude: [
+                "./mbedtls/yotta/",
+                "./mbedtls/visualc",
+                "LICENSE"
+            ],
+
+            resources: [
+                .copy("."),
+                // .copy("include/"),
+                // .copy("mbedtls"),
+                // .copy("README.md"),
+                // .copy("Makefile")
+            ],
+            // publicHeadersPath: "./mbedtls/include/",
+            cSettings: [
+                .headerSearchPath("./mbedtls/include/")
+                ]
+        ),
+        .target(
+            name: "Camiitool",
+            dependencies: ["amiitool"],
+
+            path: "./Sources/Camiitool"
             ),
         .testTarget(
             name: "CamiitoolTests",
-            dependencies: ["Camiitool"],
-            ),
+            dependencies: ["Camiitool"]
+        )
     ]
 )
